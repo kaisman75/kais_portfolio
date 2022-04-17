8acfe4d91eb05f55c84e715d6b5b78d51e8f8e47
kaisman75 <kaismanai1975gmail.com>
2022-04-17 12:05:26 +0100

    portfolio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 
-import './App.css';
+import './App.scss';
 import {About,Footer,Header,Works,Skills} from "./containers"
-
+import{Navbar} from "./components"
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+    <Navbar/>  
     <About/>
     <Header/>
     <Skills/>
